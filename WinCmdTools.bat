110b20483b5635832888fe36276089ef30c02585
Al.exe <77492646+AlexeyLepov@users.noreply.github.com>
2023-08-22 10:56:04 +0900

    Create WinCmdTools.bat

---
+++ WinCmdTools.bat
@@ -0,0 +1,46 @@
+@echo off
+
+
+:menu
+cls
+echo ===============================
+echo      Windows system helper     
+echo ===============================
+echo Choose an option:
+echo [1] Check System Information
+echo [2] Display IP Configuration
+echo [3] Display User Accounts
+echo [0] Exit
+echo ===============================
+set /p option=Choose an option: 
+if "%option%"=="1" goto sysinfo
+if "%option%"=="2" goto ipconfig
+if "%option%"=="3" goto useraccounts
+if "%option%"=="0" goto exit
+echo Invalid Option, Press any key to return to the menu.
+pause
+goto menu
+
+
+:sysinfo
+systeminfo
+pause
+goto menu
+
+
+:ipconfig
+ipconfig /all
+pause
+goto menu
+
+
+:useraccounts
+net user
+pause
+goto menu
+
+
+:exit
+echo Bye /0w0|                                   
+timeout /t 1 /nobreak
+exit
