8d094374e9ef3be8bd880adec11a2e2eb53c4ff2
Al.exe <77492646+AlexeyLepov@users.noreply.github.com>
2023-08-22 11:16:53 +0900

    Update WinCmdTools.bat
    Added features:
    
    - Check Network Connections;
    - Show Firewall Status.

--- WinCmdTools.bat
+++ WinCmdTools.bat
@@ -10,12 +10,16 @@ echo Choose an option:
 echo [1] Check System Information
 echo [2] Display IP Configuration
 echo [3] Display User Accounts
+echo [4] Check Network Connections
+echo [5] Show Firewall Status
 echo [0] Exit
 echo ===============================
 set /p option=Choose an option: 
 if "%option%"=="1" goto sysinfo
 if "%option%"=="2" goto ipconfig
 if "%option%"=="3" goto useraccounts
+if "%option%"=="4" goto networkconnections
+if "%option%"=="5" goto firewallstatus
 if "%option%"=="0" goto exit
 echo Invalid Option, Press any key to return to the menu.
 pause
@@ -36,6 +40,18 @@ goto menu
 
 :useraccounts
 net user
+pause
+goto menu
+
+
+:networkconnections
+netstat -an
+pause
+goto menu
+
+
+:firewallstatus
+netsh advfirewall show allprofiles
 pause
 goto menu
 
